7468165b4f6513df51084df9feb0d944a0832370
Rizller Souza <rizller@hotmail.com>
2025-11-03 13:58:53 -0300

    Refactor game logic and components: integrate GameBoard and Log, enhance Player component with active state

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,7 +1,7 @@
 import { use } from 'react';
 import {useState} from 'react';
 
-export default function Player({ initialName, symbol }) {
+export default function Player({ initialName, symbol, isActive }) {
     const[playerName, setPlayerName] = useState(initialName);
     const [isEditing, setIsEditing] = useState(false);
 
@@ -20,7 +20,7 @@ export default function Player({ initialName, symbol }) {
     }
 
   return (
-    <li>
+    <li className={isActive ? 'active' : undefined}>
       <span className="player">
         {isEditing ? (
             <input type="text" value={playerName}  required onChange={handleChange} />
